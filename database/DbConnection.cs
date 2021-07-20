8a851eb5c8597b09390683022543a171a0e41430
collinwillisdev <87679636+collinwillisdev@users.noreply.github.com>
2021-07-19 20:06:02 -0500

    Add files via upload

---
+++ database/DbConnection.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace Database_Connect
+{
+
+    class DbConnection
+    {
+       
+        //TODO COMPLETE METHODS
+        //TODO TEST METHODS
+        //TODO DESIGN AN ErrorOccured() METHOD THAT RETURNS TRUE IF AN ERROR HAS OCCOURED SINCE THE LAST TIME THE FUNCTION WAS RAN.
+        
+
+        
+
+        private string _connectionString;
+        private SqlConnection _connection;
+        private List<string> _errorList = new List<string>();
+
+        public DbConnection(string newConnString)
+        {
+            _connectionString = newConnString;
+            _connection = new SqlConnection(_connectionString);
+        }//end constructor
+
+        private bool BuildConnection()
+        {
+            try
+            {
+
+                _connection.Open();
+            }
+            catch (Exception ex)
+            {
+                //log error
+                return false;
+            }//end try
+
+            return true;
+        }//end method
+        private bool CloseConnection()
+        {
+            try
+            {
+                _connection.Close();
+            }
+            catch (Exception ex)
+            {
+                _errorList.Add($"ERROR {_errorList.Count + 1} -->  " + ex.Message);
+                return false;
+            }//end try
+
+            return true;
+        }//end method
+
+        public List<object[]> ExecuteQuery(string queryString)
+        {
+            //BUILD SQL COMMAND OBJECT INSTANCE
+            SqlCommand command = new SqlCommand(queryString, _connection);
+            List<object[]> resultList = new List<object[]>();
+            //OPEN DATABASE CONNECTION
+            if (BuildConnection() == false)
+            {
+            
+
+                return resultList; //empty list of array
+            }//end if
+
+            //TRY TO CONNECT TO DB
+            try
+            {
+                //EXEUCTE A DABATASE READER
+                SqlDataReader dataReaderResults = command.ExecuteReader();
+
+               
+                //GET & ITERATE THROUGH RESULTS
+                while (dataReaderResults.Read())
+                {
+                    //add those results to the list
+                    for (int index = 0; index < dataReaderResults.FieldCount; index++)
+                    {
+                        object[] aryTemp = new object[6];
+                        for (int count = 0; count < 6; count++)
+                        {
+                            aryTemp[count] = dataReaderResults[index];
+                            index++;
+                        }
+                        resultList.Add(aryTemp);
+                       
+                    }//end for
+
+                }//end while
+
+            }
+            catch (Exception ex)
+            {
+                _errorList.Add($"ERROR {_errorList.Count + 1} -->  " + ex.Message);
+            }//end try
+
+            CloseConnection();
+
+            return resultList; //List of results
+
+        }//end function
+    
+        public void ExecuteScalar(string queryString)
+        {
+            //OPEN DATABASE CONNECTION
+            if (BuildConnection() == false)
+            {
+                Console.Write("error connection failed");
+            }//end if
+
+
+            try
+            {
+                SqlCommand command = new SqlCommand(queryString, _connection);
+                object dataReaderResults = (object)command.ExecuteScalar();
+                Console.Write(dataReaderResults);
+            }
+            catch (Exception ex)
+            {
+                _errorList.Add($"ERROR {_errorList.Count + 1} -->  " + ex.Message);
+            }//end try
+
+            CloseConnection();
+
+           
+        }//end function
+
+        public void ExecuteNonQuery(string queryString)
+        {
+            int dataReaderResults;
+            if (BuildConnection() == false)
+            {
+                Console.Write("error connection failed");
+            }//end if
+
+
+            try
+            {
+                SqlCommand command = new SqlCommand(queryString, _connection);
+                dataReaderResults = command.ExecuteNonQuery();
+                Console.Write(dataReaderResults);
+            }
+            catch (Exception ex)
+            {
+                _errorList.Add($"ERROR {_errorList.Count + 1} -->  " + ex.Message);
+            }//end try
+
+            CloseConnection();
+
+            
+
+        }//end function
+
+
+         public string GetErrors()
+          {
+            string errors = "";
+            for (int index = 0; index < _errorList.Count; index++)
+            {
+                errors = string.Format(_errorList[index] + "\n");
+            }
+            return errors;
+            //return a string showing all errors
+        }
+    
+    
+         public string ClearErrors()
+         {
+            _errorList.Clear();
+            return "errors have been cleared";
+            //clear error list
+        }
+    
+      public string LogErrors(string filepath)
+     {
+            StreamWriter file = new StreamWriter(filepath);
+            for (int index = 0; index < _errorList.Count; index++)
+            {
+                file.WriteLine(_errorList[index] + "\n");
+            }
+            return "errors have been logged";
+
+            file.Close();
+            //write errors to a text file
+        }
+
+
+
+
+    }
+}
---
+++ database/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Database_Connect
+{
+    class Program
+    {
+
+
+
+        static string ConnectionString = "Data Source=DESKTOP-O5TE4OQ\\SQLEXPRESS;Initial Catalog = Test; Integrated Security = True";
+
+        static void Main(string[] args)
+        {
+            //string firstName = "";
+            //string lastName = "";
+            //
+            //string userInput = "";
+           
+
+
+            DbConnection data = new DbConnection(ConnectionString);
+            Console.Write(" This is your Scaler :");
+            data.ExecuteScalar("select * from customer;");
+            Console.WriteLine();
+
+            List<object[]> resultList = data.ExecuteQuery("select * from customer;");
+
+            Console.Write("Rows Affected");
+            data.ExecuteNonQuery("UPDATE Customer SET FirstName = 'Collin' WHERE CustomerId = '2004';");
+      
+            Console.WriteLine();
+
+            for (int index = 0; index < resultList.Count(); index++)
+            {
+                object[] aryTemp = resultList[index];
+                for (int count = 0; count < aryTemp.Length; count++)
+                {
+                    Console.Write(aryTemp[count]+ "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+
+           //Console.Write("Please enter a first name (just leave it blank for no first name): ");
+           //firstName = Console.ReadLine();
+           //
+           //
+           //
+           //Console.Write("Please enter a last name: ");
+           //lastName = Console.ReadLine();
+           //
+           //PrintDatabaseTable("Customer", firstName, lastName);
+           //
+           //
+           //
+           //Console.WriteLine();
+
+
+
+
+            Console.ReadKey();
+
+        }
+
+
+
+        static void PrintDatabaseTable(string tableName, string firstName, string lastName)
+        {
+            //establish connection
+            SqlConnection connection = new SqlConnection(ConnectionString);
+
+
+
+            //build string to hold sql
+            string sqlCommanString = "";
+            if (firstName == "")
+            {
+                sqlCommanString = string.Format("SELECT * FROM {0} WHERE LastName = '{1}'", tableName, lastName);
+            }
+            else
+            {
+                sqlCommanString = string.Format("SELECT * FROM {0} WHERE FirstName = '{1}' and LastName = '{2}'", tableName, firstName, lastName);
+            }
+
+
+
+
+            //build sql command object instance
+            SqlCommand command = new SqlCommand(sqlCommanString, connection);
+
+
+
+            //open database connection
+            connection.Open();
+            //try to connect to database
+            try
+            {
+                //execute a database reader
+                SqlDataReader dataReaderResults = command.ExecuteReader();
+                //get & iterate through results
+                while (dataReaderResults.Read())
+                {
+                    //print all fields of one record
+                    for (int index = 0; index < dataReaderResults.FieldCount; index++)
+                    {
+                        Console.Write(dataReaderResults[index] + "\t");
+                    }//end for
+
+
+
+                    Console.WriteLine();
+                }//end while
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("ERROR INTERACTING WITH DATABASE");
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+
+
+            //close connection to database
+            connection.Close();
+        }
+    }
+}
